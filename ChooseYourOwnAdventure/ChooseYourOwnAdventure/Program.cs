ec28a7de228c599720bf9182315d39562600a54e
tprange11 <tprange@gmail.com>
2018-03-22 08:48:06 -0500

    Initializing new project.

---
+++ ChooseYourOwnAdventure/ChooseYourOwnAdventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChooseYourOwnAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
